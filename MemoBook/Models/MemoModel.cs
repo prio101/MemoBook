67723352f95cbc6d3a22c8d66d47de42e89a6da1
Mahabub <miprio101@gmail.com>
2020-03-31 11:49:38 +0600

    Add project files.

---
+++ MemoBook/Models/MemoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemoBook.Models
+{
+    public class MemoModel
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Memo { get; set; }
+        public DateTime DateTime { get; set; }
+
+
+    }
+}
