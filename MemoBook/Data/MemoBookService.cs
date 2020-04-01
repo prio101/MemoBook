4f0f3be88c4db8462aabb006fb0497a2896a2876
Mahabub <miprio101@gmail.com>
2020-04-01 14:40:04 +0600

    Added the service and mockup Listing

---
+++ MemoBook/Data/MemoBookService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MemoBook.Models;
+
+
+namespace MemoBook.Data
+{
+    public class MemoBookService
+    {
+        public Task<MemoModel[]> GetMemosAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 6).Select(index => new MemoModel
+            {
+                Title = "Title",
+                Memo = "Memos",
+                DateTime = startDate.AddDays(index)
+
+            }).ToArray());
+        }
+    }
+}
--- MemoBook/Startup.cs
+++ MemoBook/Startup.cs
@@ -29,6 +29,7 @@ namespace MemoBook
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<MemoBookService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
